96f050898ef8b97c93061e39e0e994e4ca110708
hollow-purple12 <103302612+hollow-purple12@users.noreply.github.com>
2025-10-23 13:48:12 +0530

    Added all components to App.jsx

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,6 +1,33 @@
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+} 
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part.name} {props.part.exercises}
+        </p>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <>
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
+      </>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p> Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      </p>
+    )
+}
 
 const App = () => {
   const course = 'Half Stack application development'
