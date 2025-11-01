57d979cfb01acf00083be1de3cc97162ec8c732d
hollow-purple12 <103302612+hollow-purple12@users.noreply.github.com>
2025-11-01 10:33:23 +0530

    new file:   part1/course-info/src/App.css modified:   part1/course-info/src/App.jsx

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,3 +1,5 @@
+import "./App.css"
+
 const Header = (props) => {
     return (
         <h1>{props.course}</h1>
@@ -30,7 +32,7 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = { name: 'Half Stack application development',
+  const course = { name: 'Half stack application development',
     parts: [
     {name: 'Fundamentals of React', exercises: 10},
     {name: 'Using props to pass data', exercises: 7},
