388a96e1b745e139357a50e027591561c85fa801
hollow-purple12 <103302612+hollow-purple12@users.noreply.github.com>
2026-01-12 16:04:04 +0530

    new file:   part4/blog-list/tests/test_helper.js

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -7,21 +7,6 @@ const app = require('../app')
 
 const api = supertest(app)
 
-const initialBlogs = [
-    {"title":"React patterns",
-    "author":"Hange Zoë",
-    "url":"https://titansrwonderful.com/",
-    "likes":7},
-    {"title":"Type wars",
-    "author":"Orange Cat",
-    "url":"https://meow.com",
-    "likes":2},
-    {"title":"Cat wars",
-    "author":"Orange Cat",
-    "url":"https://meow.com",
-    "likes":20}
-]
-
 beforeEach(async () => {
 await Blog.deleteMany({}) 
     await Promise.all(
---
+++ part4/blog-list/tests/test_helper.js
@@ -0,0 +1,17 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {"title":"React patterns",
+    "author":"Hange Zoë",
+    "url":"https://titansrwonderful.com/",
+    "likes":7},
+    {"title":"Type wars",
+    "author":"Orange Cat",
+    "url":"https://meow.com",
+    "likes":2},
+    {"title":"Cat wars",
+    "author":"Orange Cat",
+    "url":"https://meow.com",
+    "likes":20}
+]
+
