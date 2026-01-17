7bb6308d2d4adbc466e461139e893b13b49fd0eb
Shreya <shreya501711@gmail.com>
2026-01-17 19:30:30 +0530

    blog creation allowed only to logged in users

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({})
@@ -19,11 +20,32 @@ blogsRouter.get('/:id', async (req, res, next) => {
     }
 })
 
+const getTokenFrom = req => {
+  const auth = req.get('authorization')
+  if (auth && auth.startsWith('Bearer ')) {
+    return auth.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (req, res, next) => {
   const body = req.body
+  const token = getTokenFrom(req)
 
-  const user = await User.findOne()
-  console.log(user)
+  if (!token) {
+      return res.status(401).json({ error: 'token missing' })
+  }
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  if (!user) {
+     res.status(400).json({ error: 'User Id missing or invalid' })
+  }
 
   if (!body) {
     return res.status(400).json({
@@ -55,6 +77,7 @@ blogsRouter.post('/', async (req, res, next) => {
   url: body.url,
   likes: body.likes || 0,
   user: user._id })
+
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
