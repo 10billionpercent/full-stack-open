dd0becec017e5cba37adbe955a15c97ce2418672
Shreya <shreya501711@gmail.com>
2026-01-22 22:17:54 +0530

    interchange heading order

--- part5/blog-list-frontend/src/components/Blogs.jsx
+++ part5/blog-list-frontend/src/components/Blogs.jsx
@@ -2,8 +2,8 @@ const Blogs = (props) => {
     console.log(props.blogs)
     return (
         <>
-        <h2>Blogs</h2>
         <h2>{props.name} logged in </h2>
+        <h2>Blogs</h2>
       <div>
         {props.blogs.map(blog => <p key={blog.id}>{blog.title} </p>)}
         </div>
