c28b9fd71088c5bc47dc55677709e16e4fe891b1
Shreya <shreya501711@gmail.com>
2026-01-25 18:06:14 +0530

    changed app styles and added show/hide button for blog details

---
+++ part5/blog-list-frontend/src/components/Blog.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+         const [visible, setVisible] = useState(false)
+        
+            const showWhenVisible = { 
+              display: visible ? 'flex' : 'none',
+              flexDirection: 'column',
+              width: 'fit-content'
+             }
+            const buttonLabel =  visible ? 'hide' : 'show'
+        
+            const toggleVisiblity = () => {
+                setVisible(!visible)
+            }
+        
+            return (
+                <div style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.678)' ,
+                  padding: '2px'
+                }}>
+                <div style= {{ display: 'flex', 
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  gap: '12px'
+              }}> 
+                  <h4> {blog.title} </h4>  
+                  <button onClick={toggleVisiblity}> {buttonLabel} </button>
+                </div>
+                <div style={showWhenVisible}>
+                <p> <b> url </b> {blog.url} </p> 
+                <p> <b> likes </b> {blog.likes} <button> like </button></p>  
+                <p> <b> author </b> {blog.author} </p>
+                </div>
+                </div>
+
+)
+}
+export default Blog 
--- part5/blog-list-frontend/src/components/Blogs.jsx
+++ part5/blog-list-frontend/src/components/Blogs.jsx
@@ -1,3 +1,5 @@
+import Blog from "./Blog"
+
 const Blogs = ({
     name,
     logoutHandler,
@@ -8,7 +10,7 @@ const Blogs = ({
         <h4>{name} logged in <button onClick={() => logoutHandler()}> logout </button> </h4>
         <h2>Blogs</h2>
     <ul>
-    {blogs.map(blog => <p key={blog.id}>{blog.title} </p>)}
+    {blogs.map(blog => <Blog key={ blog.id } blog={ blog }/>)}
     </ul>
         </>
     )
