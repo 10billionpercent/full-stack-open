94615cb5ceb270dcff51d26e7ee6a4bffc50d88c
Shreya <shreya501711@gmail.com>
2026-01-25 10:58:25 +0530

    ul added in Blogs

--- part5/blog-list-frontend/src/components/Blogs.jsx
+++ part5/blog-list-frontend/src/components/Blogs.jsx
@@ -7,9 +7,9 @@ const Blogs = ({
         <>
         <h4>{name} logged in <button onClick={() => logoutHandler()}> logout </button> </h4>
         <h2>Blogs</h2>
-      <div>
-        {blogs.map(blog => <p key={blog.id}>{blog.title} </p>)}
-        </div>
+    <ul>
+    {blogs.map(blog => <p key={blog.id}>{blog.title} </p>)}
+    </ul>
         </>
     )
 }
