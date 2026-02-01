6673ce476763bb8f510c3e63f8b041fda35e19ea
Shreya <shreya501711@gmail.com>
2026-02-01 14:00:46 +0530

    test to confirm that login page is shown by default

--- part5/e2e-tests/tests/bloglist.spec.js
+++ part5/e2e-tests/tests/bloglist.spec.js
@@ -2,5 +2,15 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 const { loginWith } = require('./helper')
 
 describe('bloglist', () => {
-    
+    beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173')
+    })
+
+    test('login form is shown', async ({ page }) => {
+        const locator = page.getByText('Bloglist')
+        await expect(locator).toBeVisible()
+        await expect(
+            page.getByRole('heading',{ name: 'Login' })
+        ).toBeVisible()
+    })
 })
