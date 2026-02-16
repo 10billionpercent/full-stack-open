dacabcac1855b653c735e0a853a24ddcb989625b
Shreya <shreya501711@gmail.com>
2026-02-16 15:39:20 +0530

    reducer configured with configureStore

--- part6/anecdotes-redux/src/main.jsx
+++ part6/anecdotes-redux/src/main.jsx
@@ -1,17 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createStore, combineReducers } from 'redux'
-import anecdoteReducer from './reducers/anecdoteReducer.js'
-import filterReducer from './reducers/filterReducer.js'
 import { Provider } from 'react-redux'
-
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer)
+import store from './store.js'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
