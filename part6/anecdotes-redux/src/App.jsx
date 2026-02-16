cbd97cc63742261d6269474dca06c165e89f5883
Shreya <shreya501711@gmail.com>
2026-02-16 20:47:49 +0530

    fetching anecdotes from db.json on application startup

--- part6/anecdotes-redux/src/App.jsx
+++ part6/anecdotes-redux/src/App.jsx
@@ -1,12 +1,21 @@
+import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
 import AnecdoteForm from "./components/AnecdoteForm"
 import AnecdoteList from "./components/AnecdoteList"
 import Filter from "./components/Filter"
 import Notification from "./components/Notification"
 import './App.css'
-import { useSelector } from "react-redux"
+import { setAnecdotes } from "./reducers/anecdoteReducer"
+import anecdoteService from "./services/anecdotes"
 
 const App = () => {
   const message = useSelector(state => state.notification)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <h1> Software Engineering Anecdotes </h1>
--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -22,9 +22,12 @@ const anecdoteSlice = createSlice({
                 votes: anecdoteToVote.votes + 1
             }
             return state.map(a => (a.id !== id ? a : votedAnecdote))
+        },
+        setAnecdotes(state, action) {
+            return action.payload
         }
     }
 })
 
-export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+export const { createAnecdote, voteAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
