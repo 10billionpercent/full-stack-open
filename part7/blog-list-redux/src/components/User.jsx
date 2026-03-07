a3d2849f843207d06131404b13972a34abb83cd1
Shreya <shreya501711@gmail.com>
2026-03-07 11:08:00 +0530

    users improved with tailwind

--- part7/blog-list-redux/src/components/User.jsx
+++ part7/blog-list-redux/src/components/User.jsx
@@ -1,15 +1,27 @@
+import { BsPersonLinesFill, BsArrowRight } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+
 const User = ({ user }) => {
   if (!user) {
     return null
   }
   const blogs = user.blogs
   return (
-    <div>
-      <h2> {user.name} </h2>
-      <h4> added blogs</h4>
+    <div className="flex  flex-col gap-4 px-4">
+      <div className="flex gap-4 items-center">
+        <BsPersonLinesFill className="text-3xl text-blue-300" />
+        <h2 className="font-bold text-3xl text-blue-300"> {user.name}</h2>
+      </div>
+      <h4 className="text-2xl font-bold"> added blogs</h4>
       <ul>
         {blogs.map((b) => (
-          <li key={b.id}>{b.title}</li>
+          <Link
+            key={b.id}
+            to={`/blogs/${b.id}`}
+            className="flex flex-row gap-4 items-center font-bold text-lg"
+          >
+            {b.title} <BsArrowRight />
+          </Link>
         ))}
       </ul>
     </div>
--- part7/blog-list-redux/src/components/Users.jsx
+++ part7/blog-list-redux/src/components/Users.jsx
@@ -2,27 +2,29 @@ import { Link } from 'react-router-dom'
 
 const Users = ({ users }) => {
   return (
-    <>
-      <h2>Users</h2>
-      <table>
+    <div className="flex flex-col gap-4 px-4">
+      <h2 className="font-bold text-3xl text-blue-300">Users</h2>
+      <table className="max-w-fit">
         <thead>
           <tr>
-            <th> user</th>
-            <th> blogs created</th>
+            <th className="text-left font-bold text-lg pr-8"> user</th>
+            <th className="text-left font-bold text-lg"> blogs created</th>
           </tr>
         </thead>
         <tbody>
           {users.map((u) => (
             <tr key={u.id}>
               <td>
-                <Link to={`/users/${u.id}`}> {u.name} </Link>
+                <Link to={`/users/${u.id}`} className="font-bold text-lg pr-8">
+                  {u.name}
+                </Link>
               </td>
               <td> {u.blogs.length}</td>
             </tr>
           ))}
         </tbody>
       </table>
-    </>
+    </div>
   )
 }
 export default Users
